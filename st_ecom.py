63524e4045ac5c6e03841f8d6ed73e7be9799ad5
park black <parkblak84@gmail.com>
2025-01-18 11:19:18 +0530

    Create st_ecom.py
    my first presentation here(18.01.25)

---
+++ st_ecom.py
@@ -0,0 +1,314 @@
+
+raw_product = [
+    # for bags use "bg" tag, "acs" for accessories,hd - is for bracelet, nk is for necklace
+    {
+        "id": 'mxhd01',
+        "name": "Purple-white Bracelet",
+        "price": 119,
+        "description": "Wearable with any dress/saree/jeans or gift your friend or special ones as well as colleagues.",
+        "face_pic": "IMG-20241201-WA0006.jpg"
+    },
+    {
+        "id": 'mxhd02',
+        "name": "Pink butterfly bracelet",
+        "price": 79,
+        "description": "perfect for esthetic vibes.",
+        "face_pic": "IMG-20241214-WA0002.jpg"
+    },
+    {
+        "id": 'mxnk03',
+        "name": "blue trio necklace",
+        "price": 169,
+        "description": "wearable with any classy looks.",
+        "type": "nk",
+        "face_pic": "IMG-20241214-WA0003.jpg"
+    },
+    {
+        "id": 'mxhd05',
+        "name": "pink-white bracelet",
+        "price": 129,
+        "description": "simple though elegant.",
+        "face_pic":"IMG-20241214-WA0001.jpg"
+    },
+    {
+        "id": 'mxnk06',
+        "name": "red-white necklace",
+        "price": 109,
+        "description": "charms of red with simplicity of white",
+        "face_pic": "IMG-20241214-WA0004.jpg"
+    },
+    {
+        "id": 'mxnk07',
+        "name": "pink rock necklace",
+        "price": 99,
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20241214-WA0000.jpg"
+    },
+    {
+        "id": 'mxac08',
+        "name": "pink rock necklace",
+        "price": 99,
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20250116-WA0007.jpg"
+    },
+    {
+        "id": 'mxnk09',
+        "name": "pink rock necklace",
+        "price": 99,
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20250116-WA0008.jpg"
+    },
+    {
+        "id": 'mxac10',
+        "name": "pink rock necklace",
+        "price": 99,
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20250116-WA0009.jpg"
+    },
+    {
+        "id": 'mxac11',
+        "name": "key-chains",
+        "price": 99,
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20250116-WA0010.jpg"
+    },
+    {
+        "id": 'mxac12',
+        "name": "key-chains",
+        "price": 99,
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20250117-WA0018.jpg"
+    },
+    {
+        "id": 'mxbg13',
+        "name": "pearl beeds bag",
+        "price": '__',
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20250116-WA0003.jpg"
+    },
+{
+        "id": 'mxbg14',
+        "name": "pearl beeds bag",
+        "price": '__',
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20250116-WA0004.jpg"
+    },
+{
+        "id": 'mxbg15',
+        "name": "pearl beeds bag",
+        "price": '__',
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20250116-WA0001.jpg"
+    },
+{
+        "id": 'mxbg16',
+        "name": "pearl beeds bag",
+        "price": '__',
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20250116-WA0005.jpg"
+    },
+{
+        "id": 'mxbg17',
+        "name": "pearl beeds bag",
+        "price": '__',
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "IMG-20250116-WA0002.jpg"
+    },
+{
+        "id": 'mxbg18',
+        "name": "pearl beeds bag",
+        "price": '__',
+        "description": "added pink gradient rock for added beauty",
+        "face_pic": "image_search_1737177523515.jpg"
+    },
+# {
+#         "id": 'mxac19',
+#         "name": "pearl beeds bag",
+#         "price": '__',
+#         "description": "added pink gradient rock for added beauty",
+#         "face_pic": "image_search_1737177523515.jpg"
+#     },
+
+
+]
+import streamlit as st,math,time
+# head ={'API_key' : '6716e49d6e0b4d39e2ba5843a050a4db66be2dd2e32a76b94e23e7e9ca748f91'}
+def navigate(page):  # Define navigation function
+    st.session_state.page = page
+    st.rerun()
+
+# Function for the landing page
+def landing_page():
+    st.markdown(
+        """
+        <style>
+        .center {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f5f5f5;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        '<div class="center">'
+        '<img src="https://raw.githubusercontent.com/PB-byt/PB-byt/main/IMG-20250112-WA0004.jpg" alt="Logo" width="200">'
+        '</div>',
+        unsafe_allow_html=True
+    )
+
+def main():# Main function to run the app,Initialize session state for page navigation
+    st.set_page_config(layout="centered")
+    tab_h,tab_c = st.tabs(["Home","Custom"])
+    with tab_h:
+        if 'page' not in st.session_state:
+            st.session_state.page = 'home'
+
+        # Home Page: Categories of products
+        if st.session_state.page == 'home':
+            st.header("Welcome to -")
+            # st.title('to') ⟫⁙※◈
+            st.title("‖D'R JEWELS")
+            st.write(f'<h4 style="color:pink;">◈ Hand-made with love. (✿◡‿◡) </h2>',unsafe_allow_html=True)
+            st.write(f'<h5 style="color:pink;">◈   and fully customizable</h2>', unsafe_allow_html=True)
+            categories = ['BRACELETS', 'NECKLACES', 'BAGS']  # Example categories
+            ctg_pics = ["image_search_1734490759482.jpg","image_search_1734490841505.jpg","image_search_1734493092106.jpg"]
+            st.divider()
+            st.divider()
+            for i in range(0,3):
+                with st.container(height=235):
+                    # st.header(categories[i])
+                    # st.image(ctg_pics[i])
+                    st.image(f"https://raw.githubusercontent.com/PB-byt/ecom_product_pics/main/{ctg_pics[i]}")
+                a,b,c = st.columns(3)
+                a.markdown(f'<h4 style="color:#e86d8a;">| {categories[i]} |</h2>',unsafe_allow_html=True)
+                a.markdown('<h3 style="color:pink;">starting at just *₹99*</h3>',unsafe_allow_html=True)
+                if c.button(" Shop  Now ",key = f"shop {categories[i]}"):
+                    st.session_state.category = categories[i]
+                    navigate('product_list')
+                st.divider()
+
+            with st.container(height=235):
+                st.image("https://raw.githubusercontent.com/PB-byt/ecom_product_pics/main/image_search_1734607612017.jpg")
+            a,b,c = st.columns(3)
+            a.markdown('<h4 style="color:#e86d8a;">| ACCESSORIES |</h2>',unsafe_allow_html=True)
+            a.caption("Key-chains,Hair-clips and much more")
+            a.markdown('<h3 style="color:pink;">starting at just *₹49*</h3>',unsafe_allow_html=True)
+            if c.button(" Shop  Now ",key = f"shop accessories"):
+                st.session_state.category = "ACCESSORIES"
+                navigate('product_list')
+            st.divider()
+
+        # Product List Page
+        elif st.session_state.page == 'product_list':
+            current_catagory = st.session_state.category
+            if current_catagory == 'ACCESSORIES':
+                t = 'ac'
+            elif current_catagory == 'BRACELETS':
+                t = 'hd'
+            elif current_catagory == 'NECKLACES':
+                t = 'nk'
+            else:
+                t = 'bg'
+            left,right = st.columns([0.7,0.3],vertical_alignment='center')
+            with left :
+                st.markdown(f"<h3 style ='color:#e86d8a;'>| {st.session_state.category} | </h3>",unsafe_allow_html=True)
+            # left.title(f"Products in {st.session_state.category}")
+            with right:
+                if st.button("<- Back to Catagories"):
+                    navigate('home')
+
+            # products = ['Product 1', 'Product 2', 'Product 3']  # Example products
+            all_products = raw_product
+            products = [i for i in raw_product if i['id'][2:4] == t]
+
+            l = len(products)
+            rows = math.floor(l / 3)
+            extra = l % 3
+            for i in range(0, rows):
+                r = st.columns(3)
+                p_card = products[i * 3:i * 3 + 3]
+                for j in range(3):
+                    # p=p_card[j]
+                    col = r[j]
+                    tile = col.container(height=350)
+                    tile.subheader(p_card[j]["name"])
+                    tile.image(f"https://raw.githubusercontent.com/PB-byt/ecom_product_pics/main/{p_card[j]["face_pic"]}")
+                    tile.write(f"₹ {p_card[j]['price']} /-")
+                    if col.button("details", key=f"pr_id_{p_card[j]['id']}"):
+                        st.session_state.product=p_card[j]#["name"]
+                        navigate("product_detail")
+                    # st.session_state = p_card[j]["id"]
+
+        # Product Detail Page
+        elif st.session_state.page == 'product_detail':
+            current_pr = st.session_state.product
+            # j = current_pr['id']
+            # global j
+            st.title(f"{current_pr['name']}")
+            a,b,b2 = st.columns(3)
+            a.write('| Details |')
+            if b2.button('<- Back to Products'):
+                navigate('product_list')
+            # a.write(f"{st.session_state.product} details .",)
+            c,d = st.columns([0.3,0.7])
+            with c:
+                cont=st.container(height=350)
+                with cont:
+                    st.write("price")
+                    st.subheader(f'₹{current_pr['price']}')
+                    st.caption(f"id : {current_pr['id']}")
+                    st.divider()
+                    st.write("description")
+                    st.subheader(f'{current_pr['description']}')
+                    st.divider()
+                    if st.button('buy now'):
+                        navigate('order_placing')
+                    st.write("_through email.")
+            with d:
+                pic = st.container(height=350)
+                with pic:
+                    st.image(f"https://raw.githubusercontent.com/PB-byt/ecom_product_pics/main/{current_pr['face_pic']}")
+    # order placing page-
+        elif st.session_state.page == 'order_placing':
+            current_pro = st.session_state.product
+            st.markdown(''':rainbow[|| to place order,copy the text below and mail on__]''')
+            st.markdown(''':blue-background[⟫ drjewels99@gmail.com]''')
+            st.write("We will reach you within a few hours.🤗")
+            st.divider()
+            st.caption("copy this ↡")
+            st.write(f"◌ | {current_pro['name']} |")
+            st.write(f"▻ Product id : {current_pro['id']},")
+            st.write(f'▻ Price: {current_pro['price']} ₹ only.')
+            st.write(f"▻ Quantity : 1")
+            st.divider()
+            # ▷▻▣◊◌
+            if st.button('<- back'):
+                navigate('product_detail')
+            st.caption("Happy Shopping 🤗 and have a good day.")
+        st.write("_developed & managed by P.Black.")
+
+    with tab_c:
+        st.header('||◌Where Your Choice and Our Precision meets __')
+        st.divider()
+        st.markdown(''':rainbow[◌ Mail us at_]''')
+        st.markdown(''':blue-background[drjewels99@gmail.com]''')
+        st.divider()
+        st.subheader("※This page is designed for custom products only,if want to place a order: ")
+        st.caption('◈Just mail us on given email address given above with a photo and specific details you want.')
+        st.caption('◈We will reach you within a few hours.')
+        st.write('happy shoping 🤗')
+
+if "load_state" not in st.session_state:
+    st.session_state.load_state = False
+
+if not st.session_state.load_state:
+    landing_page()
+    time.sleep(3) # Adjust the time for the splash screen
+    st.session_state.load_state = True
+    st.rerun()
+else:
+    main()
